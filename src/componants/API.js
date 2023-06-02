ccfed30c675544c15c06fd26716a026c1bfcdea6
Your Name <shrikamlapurkar9614@gmail.com>
2023-06-02 15:58:38 +0530

    changes made

--- src/componants/API.js
+++ src/componants/API.js
@@ -1,9 +1,9 @@
-import axios from 'axios';
+import axios from "axios";
 
 export default axios.create({
-    baseURL: "http://127.0.0.1:8000/backend_api/vendors/",
-    headers: {
-        'Accept':'application/json',
-        'Content-Type':'application/json',
-    }
-})
+  baseURL: "http://127.0.0.1:8000/backend_api/vendors/",
+  headers: {
+    Accept: "application/json",
+    "Content-Type": "application/json",
+  },
+});
